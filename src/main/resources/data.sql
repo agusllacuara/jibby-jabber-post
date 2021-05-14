074814c962cce86d03142de0af019a9297d2fa34
agusl <agustin.lopezlacuara@ing.austral.edu.ar>
2021-05-14 15:02:45 -0300

    Created hello world server. Added CI.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,15 @@
-INSERT INTO posts(id, text, date, user_id) VALUES (1, "first post", 1, 1);
+create table post
+(
+    id      serial not null,
+    text    text   not null,
+    date    date   not null,
+    user_id int    not null
+);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
+INSERT INTO post(text, date, user_id) VALUES ('some-text', '12-12-2012', 1);
